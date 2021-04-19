8d0a2b011c7896716bac0faad60ac6642fa38330
loronto <46075089+loronto@users.noreply.github.com>
2021-04-19 21:48:55 +0530

    Add files via upload

---
+++ ContactUs.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+    <title>
+        Contact Us
+    </title>
+    <link rel="stylesheet" href="css_contactus.css">
+
+</head>
+<body>
+
+    <?php include "topBar.php"	?>
+    <div class="contactContainer">
+
+        Address: Steinsgate Mubong,<br>
+        123456, Melbourne,<br>
+        Australia<br><br><br><br>
+
+        Reach us out at:<br>
+        +04 987654321<br>
+        +04 946547891
+<br>
+        semplice_products@senplice.com
+<br>
+        
+        </div>
+
+
+    <?php include "followLine.php"	?>
+</body>
+</html>
